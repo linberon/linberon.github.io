81624a38ce60f62255533f88064a396d9c5c6a1c
linberon <linberon@gmail.com>
2018-04-03 16:01:35 +0100

    mailer setup+smooth scroll

---
+++ app.js
@@ -0,0 +1,13 @@
+//reset contact form after submit
+document.getElementById("contactForm").reset();
+
+//smooth scroll
+$(".navbar").click(function() {
+	event.preventDefault();
+	$("html, body").animate(
+		{
+			scrollTop: $(this.hash).offset().top - 30
+		},
+		600
+	);
+});
